cc1e04ef13bf99e4de3366282f23dadab5f84686
Akib Hossain <akib.hossain.iub@gmail.com>
2021-11-24 17:47:54 +0600

    Treatment Page responsive done

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,6 +76,57 @@
 	    }
 
 
+        // Page Details Section Click To Scroll
+        $('#page-details-section .details-list-box ul li a').on('click', function(e){
+            e.preventDefault();
+            $('#page-details-section .details-list-box ul li a').removeClass('active');
+            $(this).addClass('active');
+        })
+
+
+        // Page Margin Top
+        let headerHeight = $('header').outerHeight();
+        $('.page-margin-top').css('margin-top', headerHeight);
+
+
+        // Select all links with hashes
+        $('.smooth-scroll')
+          // Remove links that don't actually link to anything
+          .not('[href="#"]')
+          .not('[href="#0"]')
+          .click(function(event) {
+            // On-page links
+            if (
+              location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+              && 
+              location.hostname == this.hostname
+            ) {
+              // Figure out element to scroll to
+              var target = $(this.hash);
+              target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+              // Does a scroll target exist?
+              if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                  scrollTop: target.offset().top - 112
+                }, 1000, function() {
+                  // Callback after animation
+                  // Must change focus!
+                  var $target = $(target);
+                  $target.focus();
+                  if ($target.is(":focus")) { // Checking if the target was focused
+                    return false;
+                  } else {
+                    $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                    $target.focus(); // Set focus again
+                  };
+                });
+              }
+            }
+          });
+
+
 	    // Testimonials Carousel
         $("#testimonials-carousel").owlCarousel({
             loop: true,
@@ -112,15 +163,18 @@
             margin: 36,
             responsive: {
                 0: {
-                    items: 1.5,
+                    items: 1,
                 },
                 576: {
-                    items: 2.5,
+                    items: 1.5,
                 },
                 768: {
-                    items: 2.5,
+                    items: 1.5,
                 },
                 992: {
+                    items: 2.5,
+                },
+                1200: {
                     items: 3.5,
                 }
             }
